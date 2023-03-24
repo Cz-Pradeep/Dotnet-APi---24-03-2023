d6ce1fe0888374505716ea4bdab1a309471cc38a
Cz-Pradeep <120454864+Cz-Pradeep@users.noreply.github.com>
2023-03-24 20:20:36 +0530

    dotnet APi

---
+++ Dtos/Character/AddCharacterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet_Demo.Dtos.Character
+{
+    public class AddCharacterDto
+    {
+        public string Name { get; set; } = "Frodo";
+   
+        public double longtitude { get; set; } = 51.9498;
+
+        public double lattitude { get; set; } = 4.4794;
+        public double velocity { get; set; } = 10.0;
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet_Demo.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+         public double longtitude { get; set; } = 51.9498;
+
+        public double lattitude { get; set; } = 4.4794;
+        public double velocity { get; set; } = 10.0;
+    }
+}
